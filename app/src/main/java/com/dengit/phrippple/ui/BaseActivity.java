991ac0e281df2954f6e0f5447accf8bf33550867
dengit <dancinginthebreeze@foxmail.com>
2015-12-12 15:22:36 +0800

    1, updated: uncomment unit test, analysitcs, memory leak detect

--- app/src/main/java/com/dengit/phrippple/ui/BaseActivity.java
+++ app/src/main/java/com/dengit/phrippple/ui/BaseActivity.java
@@ -9,12 +9,12 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        MobclickAgent.onResume(this);
+        MobclickAgent.onResume(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-//        MobclickAgent.onPause(this);
+        MobclickAgent.onPause(this);
     }
 }
